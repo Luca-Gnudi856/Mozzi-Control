72077c9f3aea114cae00f83638ac1ecd3737dc68
Luca-Gnudi856 <2352020@students.wits.ac.za>
2024-10-17 10:51:12 +0200

    Avoid overlap of title on webapp

--- restart-telebit.sh
+++ restart-telebit.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Ensure the necessary environment variables are set
-if [ -z "$XDG_RUNTIME_DIR" ]; then
-    export XDG_RUNTIME_DIR=/run/user/$(id -u)
-fi
+# if [ -z "$XDG_RUNTIME_DIR" ]; then
+#     export XDG_RUNTIME_DIR=/run/user/$(id -u)
+# fi
 
-# Add DBUS_SESSION_BUS_ADDRESS if it's missing
-if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
-    export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus
-fi
+# # Add DBUS_SESSION_BUS_ADDRESS if it's missing
+# if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
+#     export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus
+# fi
 
-# Restart the telebit-restart.service using systemctl --user
-systemctl --user restart telebit-restart.service
+# # Restart the telebit-restart.service using systemctl --user
+# systemctl --user restart telebit-restart.service
 
-# Check if the service restarted successfully
-if [ $? -eq 0 ]; then
-    echo "Telebit restarted successfully."
-else
-    echo "Failed to restart Telebit."
-fi
+# # Check if the service restarted successfully
+# if [ $? -eq 0 ]; then
+#     echo "Telebit restarted successfully."
+# else
+#     echo "Failed to restart Telebit."
+# fi
 
 
